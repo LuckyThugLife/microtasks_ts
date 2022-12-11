d93bb4e3a44e5094692527d08ab5ec47e265b678
Rustam Sharipov <rustam87.sharipov@gmail.com>
2022-12-11 21:20:24 +0500

    03_button save in App2

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,16 @@
-import React, {useState} from 'react';
-import {Button} from './03_button/03_button';
+import React from 'react';
 
-// import NewComponent, {topCars} from "./NewComponent";
+
+
 
 
 
 function App() {
 
-    const buttonFor1 = (subscriber:string, age:number, address:string) => {
-        console.log(subscriber,age,address)
+
+    return{
+
     }
-
-    const buttonFor2 = (subscriber:string) => {
-        console.log(subscriber)
-    }
-
-
-    return (
-        // <NewComponent topCars={topCars}/>
-        <div>
-            <Button name={"MyYouTubeChanel-1"} callBack={()=>buttonFor1("I am Vasiliy", 21, "Minsk")}/>
-            <Button name={"MyYouTubeChanel-2"} callBack={()=>buttonFor2("I am Ivan")}/>
-        </div>
-    );
 }
 
 export default App;
