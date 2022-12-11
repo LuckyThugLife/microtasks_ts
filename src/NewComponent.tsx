e196752c4506535a121f1f20c1a85ded77326113
Rustam Sharipov <rustam87.sharipov@gmail.com>
2022-12-11 16:37:16 +0500

    01_nested_components, 02_map_method

---
+++ src/NewComponent.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+
+
+export type NewComponentPropsType = {
+    // students:Array<StudentType>
+    // students:StudentType[]
+    topCars: topCarsType[]
+
+}
+
+/*type StudentType={
+  id: number,
+  name: string,
+  age:number
+
+}*/
+export type topCarsType = {
+    manufacturer: string,
+    model: string,
+
+}
+
+ export const topCars = [
+    {manufacturer: 'BMW', model: 'm5cs'},
+    {manufacturer: 'Mercedes', model: 'e63s'},
+    {manufacturer: 'Audi', model: 'rs6'}
+]
+
+export function NewComponent(props: NewComponentPropsType) {
+
+    return (
+        <tr>
+            {props.topCars.map((ObjectFromTopCarsArray, index) => {
+                return (
+                    <tr key={ObjectFromTopCarsArray.manufacturer}>
+                        <span>{ObjectFromTopCarsArray.model}</span>
+
+
+                    </tr>
+                )
+
+            })}
+        </tr>
+        /* <ul>
+           {props.students.map((ObjectFromStudentArray, index)=>{
+             return(
+                 <li key={ObjectFromStudentArray.id}>
+                     <span>{ObjectFromStudentArray.name}</span>
+                     <span> age :{ObjectFromStudentArray.age}</span>
+
+                 </li>
+             )
+
+           })}
+
+         </ul>*/
+
+    );
+}
+
+export default NewComponent;
