277fc28b30a3374f82868e189a8a7aeaf8deab70
Rustam Sharipov <rustam87.sharipov@gmail.com>
2022-12-18 22:04:33 +0500

    06_input&button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
 import React, {Fragment, useState} from 'react';
-import FilterMoney from "./05_filter_method/05_filter";
+import "./App.css"
+import FullInput from "./06_input/06_fullInput";
+import {Input} from "./06_input/input";
+import {Button} from "./06_input/button";
 
 
 function App() {
+    let [message, setMessage] = useState([
+        {message: 'message1'},
+        {message: 'message2'},
+        {message: 'message3'}
+    ])
 
-   return <FilterMoney/>
+    let [title, setTitle] = useState('')
+
+    const addMessage = (title: string) => {
+        let newMessage = {message: title}
+        setMessage([newMessage, ...message])
+    }
+
+    const callBackButtonHandler = () => {
+        addMessage(title)
+        setTitle('')
+    }
+
+    return <div className={'App'}>
+        {/*<FullInput addMessage={addMessage}/>*/}
+        <Input title={title} setTitle={setTitle}/>
+        <Button name={'+'} callBack={callBackButtonHandler}/>
+
+        {message.map((el, index) => {
+            return (
+                <div key={index}>{el.message}</div>
+            )
+        })}
+
+    </div>
 
 }
 
